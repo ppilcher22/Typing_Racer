570157031e66f9204afc7659ef693953343613a5
Phil <philcher22@gmail.com>
2022-08-21 22:57:55 +1000

    main game loop added

--- Project/main.py
+++ Project/main.py
@@ -1,1 +1,19 @@
 import pygame
+
+WIDTH, HEIGHT = 750, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+TEST_TEXT = 'The quick brown fox jumped over the lazy dog'
+pygame.display.set_caption('Typing Racer')
+
+def main():
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
