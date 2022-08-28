05feb1f59d9d9e572f2ed2ade273131e8b17a520
Phil <philcher22@gmail.com>
2022-08-28 17:22:27 +1000

    successfully display correct text, incorrect text and remaining text per line

--- Project/main.py
+++ Project/main.py
@@ -24,55 +24,43 @@ def draw_text(txt: str, txt_colour: Tuple[int, int, int], coords: Tuple[int, int
     txt_to_display = FONT_MAIN.render(txt, True, txt_colour)
     WIN.blit(txt_to_display, coords)
 
-def draw_window(correct_words: int, matched_input: str, incorrect_text: str, 
-                game_text: str, game_text_words: list[str], words_per_line: list[int]) -> None:
-    
+def draw_window(game_text_lst: list[Tuple[str, str, str]]) -> None:
     WIN.fill(WHITE)
-    # loop through all words to display
-    current_line = 0
-    current_line_text = ''
-    correct_words_display = FONT_MAIN.render('', True, GREEN)
-    for i, word in enumerate(game_text_words):
-        if i >= words_per_line[current_line]:
-            current_line =+ 1
-            current_line_text = ''
-        current_line_text += word
-        if i < correct_words:
-            correct_words_display = FONT_MAIN.render(current_line_text, True, GREEN)
-            WIN.blit(correct_words_display, (0, get_line_spacing(current_line)))
-        elif incorrect_text:
-            pass
-        else:
-            remaining_text = ''.join(game_text_words[correct_words:words_per_line[current_line]])
-            remaining_text_display = FONT_MAIN.render(remaining_text, True, BLACK)
-            WIN.blit(remaining_text_display, (correct_words_display.get_width(), get_line_spacing(current_line)))
-
-    
+    for line, (correct_text, incorrect_text, remaining_text) in enumerate(game_text_lst):
+        # display correct text
+        correct_text_disp = FONT_MAIN.render(correct_text, True, GREEN)
+        WIN.blit(correct_text_disp, (0, get_line_spacing(line)))
+        # display incorrect text
+        incorrect_text_disp = FONT_MAIN.render(incorrect_text, True, RED)
+        WIN.blit(incorrect_text_disp, (correct_text_disp.get_width(), get_line_spacing(line)))
+        # display remaining texts
+        remaining_text_disp = FONT_MAIN.render(remaining_text, True, BLACK)
+        WIN.blit(remaining_text_disp, (correct_text_disp.get_width() + incorrect_text_disp.get_width()\
+                    , get_line_spacing(line)))
     pygame.display.update()
 
 #TODO needs improving
 def get_line_spacing(current_line: int) -> int:
-    return current_line * FONT_MAIN.get_height() * 2 
+    return current_line * (FONT_MAIN.get_height() + 2) 
 
-def get_matched_text(current_word: str, current_input: str) -> Tuple[str, str]:
-    matched_text = ''
+def get_correct_text(input_text: str, game_text: str) -> Tuple[str, str, str]:
+    correct_text = ''
     incorrect_text = ''
+    remaining_text = ''
 
-    if len(current_input) != 0: 
-        for i, char in enumerate(current_input): 
-            if char != current_word[i]:
-                matched_text = current_input[:i]
-                incorrect_text = current_input[i:]
+    if len(input_text) != 0:
+        for i, char in enumerate(input_text):
+            if char != game_text[i]:
+                correct_text = input_text[:i]
+                incorrect_text = input_text[i:]
                 break
         else:
-            matched_text = current_input
-                
-    return(matched_text, incorrect_text)
+            correct_text = input_text
+    remaining_text = game_text[len(correct_text) + len(incorrect_text):]
 
-def wrap_text(game_text: str) -> list[Tuple[str, int]]:
-    font_height = FONT_MAIN.get_height()
-    line_spacing = 2
-    line_y = 0
+    return (correct_text, incorrect_text, remaining_text)
+
+def wrap_text(game_text: str) -> list[str]:
     game_text_line_list = []
 
     # loop until game_text is empty
@@ -88,8 +76,7 @@ def wrap_text(game_text: str) -> list[Tuple[str, int]]:
             i = game_text.rfind(' ', 0, i) + 1
 
         # Add the line of text to the list along with the y cords
-        game_text_line_list.append((game_text[:i], line_y))
-        line_y += font_height + line_spacing
+        game_text_line_list.append(game_text[:i])
         
         # updates the string to remove the line that has been dispalyed
         game_text = game_text[i:]
@@ -123,15 +110,10 @@ def main() -> None:
     clock = pygame.time.Clock()
     
     input_text= ''
-    correct_words: int = 0
-    game_text = get_game_text()
-    
-    #TODO - remove after testing
-    print(game_text)
-   
-    game_text_words = add_space_char(game_text.split())
-    words_per_line = get_words_per_line(wrap_text(game_text), game_text_words)
-    
+    game_text = wrap_text(get_game_text())
+    current_line = 0
+    proccessed_game_lst = [('', '' , line) for line in game_text]
+
     run = True
     while run:
         clock.tick(FPS)
@@ -144,19 +126,14 @@ def main() -> None:
                 else:
                     input_text += event.unicode
 
+        proccessed_game_lst[current_line] = get_correct_text(input_text, game_text[current_line])
         
-        # check if the current input matches the current word
-        if correct_words < len(game_text_words):
-            matched_input, incorrect_text = get_matched_text(game_text_words[correct_words], input_text)
-            if matched_input == game_text_words[correct_words]:
-                correct_words += 1
-                input_text = ''
-        else:
-            game_complete()
+        if proccessed_game_lst[current_line][0] == game_text[current_line]:
+            current_line += 1
+            input_text = ''
+        
+        draw_window(proccessed_game_lst)
 
-        draw_window(correct_words, matched_input, incorrect_text, game_text, 
-                                        game_text_words, words_per_line)
-        
     pygame.quit()
 
 if __name__ == '__main__':
